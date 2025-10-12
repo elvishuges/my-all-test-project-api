162ee81f0e355fbe771c627ed8136057b3b57879
Elvis HGS <elvishuges@hotmail.com>
2025-10-12 17:10:36 -0300

    update rotas permitidas

--- index.js
+++ index.js
@@ -13,8 +13,21 @@ const app = express();
 const PORT = process.env.PORT;
 const DATABASE_URL = process.env.DATABASE_URL;
 const DATABASE_NAME = process.env.DATABASE_NAME;
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS?.split(",") || [];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Permite requisições sem origem (ex: Postman) ou que estejam na lista
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // se precisar enviar cookies/autenticação
+  })
+);
 //databaseConnection(DATABASE_URL, DATABASE_NAME);
 app.use(express.json());
 app.use("/api", express.static("uploads"));
